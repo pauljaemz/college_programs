3995aa724d20326f31dd1be2981429ff0f5d2805
Abhishek Baiju <67792905+glaucusec@users.noreply.github.com>
2021-03-19 10:49:20 +0530

    5th file was changed.

--- DSA/5.c
+++ DSA/5.c
@@ -1,59 +1,45 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<conio.h>
-#define max 10
 
-int a[11] = { 10, 14, 19, 26, 27, 31, 33, 35, 42, 44, 0 };
-int b[10];
-
-void merging(int low, int mid, int high)
+void main()
 {
-   int l1, l2, i;
+	int a[100], b[100], m, n, i, j, k;
+	clrscr();
+	printf("Enter the number of elements in the first array :");
+	scanf("%d", &m);
+	printf("Enter the elements :");
+	for(i=0;i<m;i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	printf("Enter the elements in the second array");
+	scanf("%d", &n);
+	printf("Enter the elements\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d", &b[i]);
+	}
 
-   for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++)
-    {
-      if(a[l1] <= a[l2])
-         b[i] = a[l1++];
-      else
-         b[i] = a[l2++];
-   }
-
-   while(l1 <= mid)
-      b[i++] = a[l1++];
-
-   while(l2 <= high)
-      b[i++] = a[l2++];
-
-   for(i = low; i <= high; i++)
-      a[i] = b[i];
-}
-
-void sort(int low, int high) {
-   int mid;
-
-   if(low < high) {
-      mid = (low + high) / 2;
-      sort(low, mid);
-      sort(mid+1, high);
-      merging(low, mid, high);
-   } else {
-      return;
-   }
-}
-
-void main() {
-   int i;
-   clrscr();
-   printf("List before sorting\n");
-
-   for(i = 0; i <= max; i++)
-      printf("%d ", a[i]);
-
-   sort(0, max);
-
-   printf("\nList after sorting\n");
-
-   for(i = 0; i <= max; i++)
-      printf("%d ", a[i]);
-	  
+	for(i=0;i<m+n;i++)
+	{
+		a[m+i] = b[i];
+	}
+	for(j=0;j<(m+n)-1;j++)
+	{
+		for(k=0;k<(m+n)-j-1;k++)
+		{
+			if(a[k] > a[k+1])
+			{
+				int temp = a[k];
+				a[k] = a[k+1];
+				a[k+1] = temp;
+			}
+		}
+	}
+	printf("\nArrays after merging are :\n");
+	for(i=0;i<m+n;i++)
+	{
+		printf("%d\n", a[i]);
+	}
 	getch();
 }
